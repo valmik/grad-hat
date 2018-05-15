6d0209d717c6402478d1b0939a01a09d2126989d
valmik <rsnorunt@gmail.com>
2018-05-15 01:54:51 -0700

    tweak

--- image.py
+++ image.py
@@ -9,30 +9,43 @@ import time
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-if len(sys.argv) < 2:
-    sys.exit("require an image argument")
-else:
-    image_file = sys.argv[1]
 
-image = Image.open(image_file)
 
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 32
-options.chain_length = 4
-options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'
 
-matrix = RGBMatrix(options = options)
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("require an image argument")
+    else:
+        image_file = sys.argv[1]
 
-image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+    image = Image.open(image_file)
 
-matrix.SetImage(image.convert('RGB'))
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 32
+    options.chain_length = 4
+    options.parallel = 1
+    options.hardware_mapping = 'adafruit-hat'
 
-try:
-    print("Press CTRL-C to stop")
-    while True:
-        time.sleep(100)
-except KeyboardInterrupt:
-    sys.exit(0)
+    matrix = RGBMatrix(options = options)
+
+    image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+    matrix.SetImage(image.convert('RGB'))
+
+    try:
+        print("Press CTRL-C to stop")
+        while True:
+            time.sleep(100)
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
 
